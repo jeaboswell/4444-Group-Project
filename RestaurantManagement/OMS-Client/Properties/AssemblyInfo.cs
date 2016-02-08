352f6d0dffc491586ae05d7cd7ecabffdbe3df6b
jeaboswell <jeaboswell@gmail.com>
2016-02-08 16:38:04 -0600

    Basic project initialization

--- RestaurantManagement/OMS-Client/Properties/AssemblyInfo.cs
+++ RestaurantManagement/OMS-Client/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrderManagementSystem")]
+[assembly: AssemblyTitle("OMS-Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrderManagementSystem")]
+[assembly: AssemblyProduct("OMS-Client")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
