8c64be6fb0227111f8fe2b6b3acae57bc8f45729
jeaboswell <jeaboswell@gmail.com>
2016-02-08 21:11:57 -0600

    Working on sockets

--- RestaurantManagement/OMS-Client/Client.xaml.cs
+++ RestaurantManagement/OMS-Client/Client.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,169 @@ namespace OMS
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Client : Window
     {
-        public MainWindow()
+        public Client()
         {
             InitializeComponent();
+        }
+
+        void Connect(string serverIP, string message)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            int wantedPort = 4444;    //this is the port you want
+
+            byte[] msg = Encoding.ASCII.GetBytes("Are you my server?");
+
+
+            foreach (NetworkInterface netwIntrf in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                textBox.Text += "Interface name: " + netwIntrf.Name + "\n";
+                // Console.WriteLine("Interface name: " + netwIntrf.Name);
+
+                //textBox.Text += ("Inteface working: {0}", netwIntrf.OperationalStatus == OperationalStatus.Up);
+                Console.WriteLine("Inteface working: {0}", netwIntrf.OperationalStatus == OperationalStatus.Up);
+
+                //if the current interface doesn't have an IP, skip it
+                if (!(netwIntrf.GetIPProperties().GatewayAddresses.Count > 0))
+                {
+                    break;
+                }
+
+                //Console.WriteLine("IP Address(es):");
+
+                //get current IP Address(es)
+                foreach (UnicastIPAddressInformation uniIpInfo in netwIntrf.GetIPProperties().UnicastAddresses)
+                {
+                    //get the subnet mask and the IP address as bytes
+                    byte[] subnetMask = uniIpInfo.IPv4Mask.GetAddressBytes();
+                    byte[] ipAddr = uniIpInfo.Address.GetAddressBytes();
+
+                    // we reverse the byte-array if we are dealing with littl endian.
+                    if (BitConverter.IsLittleEndian)
+                    {
+                        Array.Reverse(subnetMask);
+                        Array.Reverse(ipAddr);
+                    }
+
+                    //we convert the subnet mask as uint (just for didactic purposes (to check everything is ok now and next - use thecalculator in programmer mode)
+                    uint maskAsInt = BitConverter.ToUInt32(subnetMask, 0);
+                    //Console.WriteLine("\t subnet={0}", Convert.ToString(maskAsInt, 2));
+
+                    //we convert the ip addres as uint (just for didactic purposes (to check everything is ok now and next - use thecalculator in programmer mode)
+                    uint ipAsInt = BitConverter.ToUInt32(ipAddr, 0);
+                    //Console.WriteLine("\t ip={0}", Convert.ToString(ipAsInt, 2));
+
+                    //we negate the subnet to determine the maximum number of host possible in this subnet
+                    uint validHostsEndingMax = ~BitConverter.ToUInt32(subnetMask, 0);
+                    //Console.WriteLine("\t !subnet={0}", Convert.ToString(validHostsEndingMax, 2));
+
+                    //we convert the start of the ip addres as uint (the part that is fixed wrt the subnet mask - from here we calculate each new address by incrementing with 1 and converting to byte[] afterwards 
+                    uint validHostsStart = BitConverter.ToUInt32(ipAddr, 0) & BitConverter.ToUInt32(subnetMask, 0);
+                    //Console.WriteLine("\t IP & subnet={0}", Convert.ToString(validHostsStart, 2));
+
+                    //we increment the startIp to the number of maximum valid hosts in this subnet and for each we check the intended port (refactoring needed)
+                    for (uint i = 1; i <= validHostsEndingMax; i++)
+                    {
+                        uint host = validHostsStart + i;
+                        //byte[] hostAsBytes = BitConverter.GetBytes(host);
+                        byte[] hostBytes = BitConverter.GetBytes(host);
+                        if (BitConverter.IsLittleEndian)
+                        {
+                            Array.Reverse(hostBytes);
+                        }
+
+                        //this is the candidate IP address in "readable format" 
+                        String ipCandidate = Convert.ToString(hostBytes[0]) + "." + Convert.ToString(hostBytes[1]) + "." + Convert.ToString(hostBytes[2]) + "." + Convert.ToString(hostBytes[3]);
+                        //Console.WriteLine("Trying: " + ipCandidate);
+                        textBox.Text += "Trying: " + ipCandidate + "\n";
+
+                        try
+                        {
+                            //try to connect
+                            sock.Connect(ipCandidate, wantedPort);
+                            if (sock.Connected == true)  // if succesful => something is listening on this port
+                            {
+                                textBox.Text += "\tIt worked at " + ipCandidate + "\n";
+                                //Console.WriteLine("\tIt worked at " + ipCandidate);
+                                sock.Close();
+                                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            }
+                            //else -. goes to exception
+                        }
+                        catch (SocketException ex)
+                        {
+                            //TODO: if you want, do smth here
+                            textBox.Text += "\tDIDN'T work at " + ipCandidate + "\n";
+                            //Console.WriteLine("\tDIDN'T work at " + ipCandidate);
+                        }
+                    }
+                }
+                Console.ReadLine();
+            }
+            sock.Close();
+            /*
+            string output = "";
+
+            try
+            {
+                // Create a TcpClient. 
+                // The client requires a TcpServer that is connected 
+                // to the same address specified by the server and port 
+                // combination.
+                Int32 port = 13;
+                TcpClient client = new TcpClient(serverIP, port);
+
+                // Translate the passed message into ASCII and store it as a byte array.
+                Byte[] data = new Byte[256];
+                data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing. 
+                // Stream stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                output = "Sent: " + message;
+                MessageBox.Show(output);
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                output = "Received: " + responseData;
+                MessageBox.Show(output);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                output = "ArgumentNullException: " + e;
+                MessageBox.Show(output);
+            }
+            catch (SocketException e)
+            {
+                output = "SocketException: " + e.ToString();
+                MessageBox.Show(output);
+            }
+            */
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // In this code example, use a hard-coded 
+            // IP address and message. 
+            string serverIP = "localhost";
+            string message = "Hello";
+            Connect(serverIP, message);
         }
     }
 }
--- RestaurantManagement/OMS-Server/Server.xaml.cs
+++ RestaurantManagement/OMS-Server/Server.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,55 @@ namespace OMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string output = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void createListener()
+        {
+            // Create an instance of the TcpListener class.
+            TcpListener tcpListener = null;
+            IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            try
+            {
+                // Set the listener on the local IP address 
+                // and specify the port.
+                tcpListener = new TcpListener(ipAddress, 4444);
+                tcpListener.Start();
+                output = "Waiting for a connection...";
+            }
+            catch (Exception e)
+            {
+                output = "Error: " + e.ToString();
+                MessageBox.Show(output);
+            }
+            while (true)
+            {
+                // Always use a Sleep call in a while(true) loop 
+                // to avoid locking up your CPU.
+                Thread.Sleep(10);
+                // Create a TCP socket. 
+                // If you ran this server on the desktop, you could use 
+                // Socket socket = tcpListener.AcceptSocket() 
+                // for greater flexibility.
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                // Read the data stream from the client. 
+                byte[] bytes = new byte[256];
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Read(bytes, 0, bytes.Length);
+                //SocketHelper helper = new SocketHelper();
+                //helper.processMsg(tcpClient, stream, bytes);
+                string mstrMessage = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                textBox.Text += mstrMessage + "\n";
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            createListener();
         }
     }
 }
