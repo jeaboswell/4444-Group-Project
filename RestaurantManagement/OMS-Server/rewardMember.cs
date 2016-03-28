92cd01a9d1ebe13e77da5f0add470f47e919ce0c
Jesse Boswell <jeaboswell@hotmail.com>
2016-03-28 13:11:15 -0500

    Fix Server Build

---
+++ RestaurantManagement/OMS-Server/rewardMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OMS
+{
+	[Serializable]
+	public class rewardMember
+	{
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public DateTime birthDate { get; private set; } // setBirthDate()
+		public string phoneNumber { get; private set; } // setPhoneNumber()
+		public string email { get; set; }
+		public string address { get; set; }
+
+		public void setBirthDate(int month, int day, int year)
+		{
+			birthDate = birthDate.AddMonths(month);
+			birthDate = birthDate.AddDays(day);
+			birthDate = birthDate.AddYears(year);
+		}
+
+		public void setPhoneNumber(string pass)
+		{
+			phoneNumber += "(" + pass[0] + pass[1] + pass[2] + ") " + pass[3] + pass[4] + pass[5] + "-" + pass[6] + pass[7] + pass[8] + pass[9];
+		}
+	}
+}
