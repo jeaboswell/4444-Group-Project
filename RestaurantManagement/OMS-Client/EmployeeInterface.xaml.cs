cf741d7eef79d93254c6999164bf7a895a23a20d
Christian Betancourt <ChristianBetancourt@my.unt.edu>
2016-04-27 13:55:10 -0500

    More updates to employee interface

--- RestaurantManagement/OMS-Client/EmployeeInterface.xaml.cs
+++ RestaurantManagement/OMS-Client/EmployeeInterface.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -35,6 +37,8 @@ namespace OMS
     /// </summary>
     public partial class EmployeeInterface : UserControl
     {
+        List<ClientInfo> TableList = new List<ClientInfo>;
+
         public EmployeeInterface()
         {
             InitializeComponent();
@@ -48,17 +52,45 @@ namespace OMS
             IPEndPoint serverEp = new IPEndPoint(tempAdr, 0);
             UdpClient server = new UdpClient(44446);
 
-            
-            
-                try
-                {
-                    byte[] command = server.Receive(ref serverEp);
 
-                }
-                catch (Exception ex) { }
+
+            try
+            {
+                byte[] command = server.Receive(ref serverEp);
+                TableList = (List<ClientInfo>)ByteToObject(command);
             }
-            server.Close();
+            catch (Exception) { }
         
+            server.Close();
+
+            foreach (ClientInfo iter in TableList)
+            {
+                Button tmpButton = new Button();
+                tmpButton.Content = iter.Name;
+                tmpButton.Click += (s, e) => { /*Add code here to determine what happens when table button is clicked*/};
+                tmpButton.Height = 100;
+                tmpButton.Width = 100;
+                Table_Grid.Children.Add(tmpButton);
+            }
+
         }
+
+
+
+        private object ByteToObject(byte[] byteArray)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream(byteArray);
+                BinaryFormatter bf = new BinaryFormatter();
+                ms.Position = 0;
+
+                return bf.Deserialize(ms);
+            }
+            catch (Exception) { }
+
+            return null;
+        }
+    }
 }
 
