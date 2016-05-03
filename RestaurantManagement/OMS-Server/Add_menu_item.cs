d778a81061437e0bab432b7ecbf604a7781b5b14
jeaboswell <jeaboswell@gmail.com>
2016-05-03 01:20:59 -0500

    Added sending pictures to server add item

--- RestaurantManagement/OMS-Server/Add_menu_item.cs
+++ RestaurantManagement/OMS-Server/Add_menu_item.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;  // System.Data.dll
+using System.IO;
 
 namespace OMS
 {
-    public partial class add_item : Form
+	public partial class add_item : Form
     {
         public add_item()
         {
@@ -25,18 +19,16 @@ namespace OMS
 
         private void add_to_DB_Click(object sender, EventArgs e)
         {
-
-
             using (SqlConnection openCon = new SqlConnection("Server=tcp:omsdb.database.windows.net,1433;Database=OMSDB;User ID=csce4444@omsdb;Password=Pineapple!;"))
             {
-                string command = "INSERT INTO dbo.Menu (ItemName, Description, Price, category)";
-                command += "VALUES (@ItemName, @Description, @Price, @category)";
+                string command = "INSERT INTO dbo.Menu (ItemName, Description, Price, category, Photo)";
+                command += "VALUES (@ItemName, @Description, @Price, @category, @Photo)";
 
                 using (SqlCommand myCommand = new SqlCommand(command, openCon))
                 {
                     myCommand.Parameters.AddWithValue("@ItemName", textBox1.Text);
                     myCommand.Parameters.AddWithValue("@Price", textBox2.Text);
-                    //myCommand.Parameters.AddWithValue("@Photo", DBNull.Value); // can't set Null like this, will have to add photo after the query i guess
+                    myCommand.Parameters.AddWithValue("@Photo", File.ReadAllBytes(textBox4.Text)); // can't set Null like this, will have to add photo after the query i guess
                     myCommand.Parameters.AddWithValue("@category", textBox5.Text);
                     myCommand.Parameters.AddWithValue("@Description", textBox3.Text);
                     openCon.Open();
@@ -45,10 +37,9 @@ namespace OMS
                 }
             }
 
-
             MessageBox.Show("Item added!");
-            //MessageBox.Show(textBox1.Text);
-            this.Close();
+			//MessageBox.Show(textBox1.Text);
+			Close();
         }
-    }
+	}
 }
